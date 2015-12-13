4d8c8f180fe4fe869a1c93ec42c6f3b712b23794
duolew <445138773@qq.com>
2015-12-13 21:29:46 +0800

    2

--- cinema/server/model/movie.js
+++ cinema/server/model/movie.js
@@ -1,7 +1,7 @@
 /**
  * Created by duole on 2015/12/13.
  */
-//导出通过模式moviceSchema生成的movie模型
+//导出通过模式moviceSchema生成的movie模型1
 var mongoose = require('mongoose')
 var movieSchema = require('../lib/schemas/movie');
 var Movie = mongoose.model('Movie1',movieSchema);   //编译生成Movie这个模型，后边传入模型名字(随便起不影响)及模式（不要引号）
